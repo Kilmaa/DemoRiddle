5fe4daefa05d8ab9d330445ea2cba5f1582e79c3
Kilmaa <20022404@myrp.edu.sg>
2021-05-19 11:09:06 +0800

    Second commit

--- app/src/main/java/sg/edu/rp/c346/id20022404/demoriddle/MainActivity.java
+++ app/src/main/java/sg/edu/rp/c346/id20022404/demoriddle/MainActivity.java
@@ -3,12 +3,28 @@ package sg.edu.rp.c346.id20022404.demoriddle;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView tvQ1;
+    Button btnRevealQ1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tvQ1 = findViewById(R.id.textVieQ1);
+        btnRevealQ1 = findViewById(R.id.buttonRevealQ1);
+
+        btnRevealQ1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
